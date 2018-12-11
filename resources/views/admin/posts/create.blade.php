04ff23f561e3a67fc6901f4d84f3e2c74194edb8
3A532055 <ylhc214044@gmail.com>
2018-12-11 20:37:45 +0800

    Validation 練習3.測試 View 上有無錯誤訊息並印在 View 上: 編輯 create.blade.php 及 edit.blade.php

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -18,14 +18,19 @@
 </div>
 <!-- /.row -->
 
+@if($errors->any())
 <div class="row">
     <div class="col-lg-12">
         <div class="alert alert-danger alert-dismissable">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
             <i class="fa fa-info-circle"></i>  <strong>警告！</strong> 請修正表單錯誤：
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
         </div>
     </div>
 </div>
+@endif
 <!-- /.row -->
 
 <div class="row">
@@ -35,7 +40,7 @@
 
             <div class="form-group">
                 <label>標題：</label>
-                <input name="title" class="form-control" placeholder="請輸入文章標題"
+                <input name="title" class="form-control" placeholder="請輸入文章標題">
             </div>
 
             <div class="form-group">
